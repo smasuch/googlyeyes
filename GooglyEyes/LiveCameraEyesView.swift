3ada7dbcf084248d3e8234dc6c2f770f42314d7c
Steven Masuch <smasuch@electricmilk.com>
2018-03-09 12:34:54 -0800

    Initial commit

---
+++ GooglyEyes/LiveCameraEyesView.swift
@@ -0,0 +1,26 @@
+//
+//  LiveCameraEyesView.swift
+//  GooglyEyes
+//
+//  Created by Steven Masuch on 2018-03-09.
+//  Copyright © 2018 Zanopan. All rights reserved.
+//
+
+import UIKit
+
+/// A view that shows a live camera image with googly eyes superposed on people's eyes
+class LiveCameraEyesView: UIView {
+  
+  func startShowingCameraFeed() {
+    // check if we've got permission
+    // get the camera image
+    // start the animation loop showing the image in the view
+  }
+  
+  func compositeImage() {
+    // get the position & size of the eyes in the image, if any
+    // take the latest camera image and draw the eyes on top
+    // put it into the view
+  }
+  
+}
---
+++ GooglyEyes/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  CameraEyesViewController.swift
+//  GooglyEyes
+//
+//  Created by Steven Masuch on 2018-03-09.
+//  Copyright © 2018 Zanopan. All rights reserved.
+//
+
+
+
+import UIKit
+
+/// The base view controller for the app. Shows a camera image with googly eyes superimposed on it.
+class CameraEyesViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
